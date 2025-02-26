ff3f8e69c1563d7dbaf756a94a0ffac95ca849f3
gaksss <clement.carlier2@gmail.com>
2025-02-26 12:00:01 +0100

    Setup api platform done

---
+++ src/Entity/Book.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Patch;
+use ApiPlatform\Metadata\Post;
+use ApiPlatform\Metadata\Delete;
+use App\Repository\BookRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
+
+#[ORM\Entity(repositoryClass: BookRepository::class)]
+#[ApiResource(
+    operations: [
+        new Get(normalizationContext: ['groups' => ['book:read']]),
+        new GetCollection(normalizationContext: ['groups' => ['book:read']]),
+        new Post(denormalizationContext: ['groups' => ['book:write']]),
+        new Patch(denormalizationContext: ['groups' => ['book:write']]),
+        new Delete(),
+    ]
+)]
+class Book
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    #[Groups(['book:read'])]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    #[Groups(['book:read', 'book:write'])]
+    private ?string $title = null;
+
+    #[ORM\Column(length: 255)]
+    #[Groups(['book:read', 'book:write'])]
+    private ?string $author = null;
+
+    /**
+     * @var Collection<int, Category>
+     */
+    // Dans Book.php, ajoutez :
+    #[ORM\ManyToMany(targetEntity: Category::class, inversedBy: 'books')]
+    #[Groups(['book:read', 'book:write'])]
+    private Collection $categories;
+
+    public function __construct()
+    {
+        $this->categories = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): static
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getAuthor(): ?string
+    {
+        return $this->author;
+    }
+
+    public function setAuthor(string $author): static
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Category>
+     */
+    public function getCategories(): Collection
+    {
+        return $this->categories;
+    }
+
+    public function addCategory(Category $category): static
+    {
+        if (!$this->categories->contains($category)) {
+            $this->categories->add($category);
+            $category->addBook($this);
+        }
+
+        return $this;
+    }
+
+    public function removeCategory(Category $category): static
+    {
+        if ($this->categories->removeElement($category)) {
+            $category->removeBook($this);
+        }
+
+        return $this;
+    }
+}
